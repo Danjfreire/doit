8927db4498e1c261f89e5fc13c83cb4da23bd353
Daniel Freire <danjfreire@gmail.com>
2024-10-23 08:25:38 -0300

    feat:add flow to delete todos

--- apps/api/src/todo/todo.controller.ts
+++ apps/api/src/todo/todo.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   NotFoundException,
   Param,
@@ -9,18 +10,28 @@ import {
 } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './create-todo.dto';
+import { Todo } from './todo.entity';
 
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
+  @Get()
+  async listTodos(): Promise<{ todos: Todo[] }> {
+    const todos = await this.todoService.list();
+    return { todos };
+  }
+
   @Post()
-  async createTodo(@Body() body: CreateTodoDto) {
+  async createTodo(@Body() body: CreateTodoDto): Promise<Todo> {
     return await this.todoService.createTodo(body);
   }
 
   @Put('/:id')
-  async updateTodo(@Param('id') id: string, @Body() body: CreateTodoDto) {
+  async updateTodo(
+    @Param('id') id: string,
+    @Body() body: CreateTodoDto,
+  ): Promise<Todo> {
     const res = await this.todoService.updateTodo(id, body);
 
     if (!res) {
@@ -30,9 +41,12 @@ export class TodoController {
     return res;
   }
 
-  @Get()
-  async listTodos() {
-    const todos = await this.todoService.list();
-    return { todos };
+  @Delete('/:id')
+  async deleteTodo(@Param('id') id: string): Promise<void> {
+    const res = await this.todoService.deleteTodo(id);
+
+    if (!res) {
+      throw new NotFoundException('todo-not-found');
+    }
   }
 }
